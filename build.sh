fbd7431eaaa7e9fdadf6cd336d6c2c7a50c85000
Miguel Sousa <mikesilva.290@gmail.com>
2022-05-19 17:50:41 -0400

    Harden binaries

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir -p dist
-go build --ldflags="-s -w" -o ./dist/ sshd.go
-go build --ldflags="-s -w" -o ./dist/ telnetd.go
-go build --ldflags="-s -w" -o ./dist/ fshell.go
+go build --ldflags="-s -w" -buildmode=pie -o ./dist/ sshd.go
+go build --ldflags="-s -w" -buildmode=pie -o ./dist/ telnetd.go
+go build --ldflags="-s -w" -buildmode=pie -o ./dist/ fshell.go
